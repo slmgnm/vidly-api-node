ad92e2ab6ab2f8879d8a0edcc6e44646e79b8106
Salim Ghanem <saleemghanem@gmail.com>
2021-03-05 13:06:18 +0200

    mongoDB cluster&User

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,19 @@
-const Joi = require('joi');
-const bcrypt = require('bcrypt');
-const _ = require('lodash');
-const {User} = require('../models/user');
-const mongoose = require('mongoose');
-const express = require('express');
+const Joi = require("joi");
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+const { User } = require("../models/user");
+const express = require("express");
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-  const { error } = validate(req.body); 
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send('Invalid email or password.');
+  if (!user) return res.status(400).send("Invalid email or password.");
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword) return res.status(400).send('Invalid email or password.');
+  if (!validPassword) return res.status(400).send("Invalid email or password.");
 
   const token = user.generateAuthToken();
   res.send(token);
@@ -29,4 +28,4 @@ function validate(req) {
   return Joi.validate(req, schema);
 }
 
-module.exports = router; 
+module.exports = router;
